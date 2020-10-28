4dea6587edae1b12f303189f72d74f6c11d75dc6
Serge <sergiy.osadchyy@gmail.com>
2020-10-28 18:49:26 +0200

    add LeftCardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeftCardComponent } from './shared/controls/left-card/left-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftCardComponent
   ],
   imports: [
     BrowserModule
