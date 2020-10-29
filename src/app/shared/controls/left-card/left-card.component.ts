4e8e0ada125a9d71cb3065bae03a73e82beeb568
Serge <sergiy.osadchyy@gmail.com>
2020-10-29 17:35:56 +0200

    add html and styles for statistic

--- src/app/shared/controls/left-card/left-card.component.ts
+++ src/app/shared/controls/left-card/left-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-left-card',
   templateUrl: './left-card.component.html',
   styleUrls: ['./left-card.component.scss']
 })
-export class LeftCardComponent implements OnInit {
+export class LeftCardComponent {
+  @Input() public numberOfStars: number = 4;
+  public stars: Array<number>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.stars = Array(this.numberOfStars).fill(0).map((x, i) => i);
   }
-
 }
