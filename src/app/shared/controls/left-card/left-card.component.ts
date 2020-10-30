9be2247f433d1a9da7aa2f229b2ac61ba8530fb6
Serge <sergiy.osadchyy@gmail.com>
2020-10-30 12:54:11 +0200

    add language collection

--- src/app/shared/controls/left-card/left-card.component.ts
+++ src/app/shared/controls/left-card/left-card.component.ts
@@ -10,6 +10,17 @@ export class LeftCardComponent {
   @Input() public description: string = 'UI\\UX Designer, Web Designer,\n' +
     'Mobile App Designer, UI\\UX Designer, Web Designer,Mobile App Designer';
   @Input() public city: string = 'Lviv, Ukraine';
+  @Input() public languages: Array<any> = [
+    {src: 'assets/images/shared/controls/left-card/ukraine.svg', alt: 'ukrainian'},
+    {src: 'assets/images/shared/controls/left-card/britain.svg', alt: 'english'},
+    {src: 'assets/images/shared/controls/left-card/russia.svg', alt: 'russian'},
+    {src: 'assets/images/shared/controls/left-card/france.svg', alt: 'french'},
+    {src: 'assets/images/shared/controls/left-card/germany.svg', alt: 'german'},
+    {src: 'assets/images/shared/controls/left-card/turkey.svg', alt: 'turkish'},
+    {src: 'assets/images/shared/controls/left-card/china.svg', alt: 'chinese'},
+    {src: 'assets/images/shared/controls/left-card/spain.svg', alt: 'spanish'},
+    {src: 'assets/images/shared/controls/left-card/portugal.svg', alt: 'portuguese'}
+  ];
 
   public stars: Array<number>;
 
