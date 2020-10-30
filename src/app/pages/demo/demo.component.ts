0e74eaa4e7d70bee71fe4e68b4e18bbbbcf84fee
Serge <sergiy.osadchyy@gmail.com>
2020-10-30 20:42:19 +0200

    refactor LeftCardComponent

--- src/app/pages/demo/demo.component.ts
+++ src/app/pages/demo/demo.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent implements OnInit {
+  public demoPerson:
+    { avatar: {src: string, alt: string },
+      name: string,
+      description: string,
+      city: string,
+      numberOfFullStars: number,
+      hasHalfStar: boolean,
+      numberOfReviews: number
+    };
+
+  public demoLanguages: Array<any>;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+  this.demoPerson = {
+      avatar: {src: 'assets/images/shared/controls/left-card/avatar.svg', alt: 'avatar'},
+      name: 'Cristofer Carder Junior',
+      description: 'UI\\UX Designer, Web Designer,\nMobile App Designer, UI\\UX Designer,\n Web Designer,Mobile App Designer',
+      city: 'Lviv, Ukraine',
+      numberOfFullStars: 4,
+      hasHalfStar: true,
+      numberOfReviews: 234
+    };
 
+  this.demoLanguages = [
+      {src: 'assets/images/shared/controls/left-card/ukraine.svg', alt: 'ukrainian'},
+      {src: 'assets/images/shared/controls/left-card/britain.svg', alt: 'english'},
+      {src: 'assets/images/shared/controls/left-card/russia.svg', alt: 'russian'},
+      {src: 'assets/images/shared/controls/left-card/france.svg', alt: 'french'},
+      {src: 'assets/images/shared/controls/left-card/germany.svg', alt: 'german'},
+      {src: 'assets/images/shared/controls/left-card/turkey.svg', alt: 'turkish'},
+      {src: 'assets/images/shared/controls/left-card/china.svg', alt: 'chinese'},
+      {src: 'assets/images/shared/controls/left-card/spain.svg', alt: 'spanish'},
+      {src: 'assets/images/shared/controls/left-card/portugal.svg', alt: 'portuguese'}
+    ];
+  }
 }
--- src/app/shared/controls/left-card/left-card.component.ts
+++ src/app/shared/controls/left-card/left-card.component.ts
@@ -1,36 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-left-card',
   templateUrl: './left-card.component.html',
   styleUrls: ['./left-card.component.scss']
 })
-export class LeftCardComponent {
-  @Input() public languages: Array<any> = [
-    {src: 'assets/images/shared/controls/left-card/ukraine.svg', alt: 'ukrainian'},
-    {src: 'assets/images/shared/controls/left-card/britain.svg', alt: 'english'},
-    {src: 'assets/images/shared/controls/left-card/russia.svg', alt: 'russian'},
-    {src: 'assets/images/shared/controls/left-card/france.svg', alt: 'french'},
-    {src: 'assets/images/shared/controls/left-card/germany.svg', alt: 'german'},
-    {src: 'assets/images/shared/controls/left-card/turkey.svg', alt: 'turkish'},
-    {src: 'assets/images/shared/controls/left-card/china.svg', alt: 'chinese'},
-    {src: 'assets/images/shared/controls/left-card/spain.svg', alt: 'spanish'},
-    {src: 'assets/images/shared/controls/left-card/portugal.svg', alt: 'portuguese'}
-  ];
+export class LeftCardComponent implements OnInit {
+  @Input() public languages: Array<any>;
 
-  @Input() public person: any = {
-    avatar: {src: 'assets/images/shared/controls/left-card/avatar.svg', alt: 'avatar'},
-    name: 'Cristofer Carder Junior',
-    description: 'UI\\UX Designer, Web Designer,\nMobile App Designer, UI\\UX Designer,\n Web Designer,Mobile App Designer',
-    city: 'Lviv, Ukraine',
-    numberOfFullStars: 4,
-    hasHalfStar: true,
-    numberOfReviews: 234
-  };
+  @Input()
+  public person:
+    { avatar: {src: string, alt: string },
+      name: string,
+      description: string,
+      city: string,
+      numberOfFullStars: number,
+      hasHalfStar: boolean,
+      numberOfReviews: number
+    };
 
   public stars: Array<number>;
 
-  constructor() {
+  constructor() { }
+
+  ngOnInit(): void {
     this.stars = Array(this.person.numberOfFullStars).fill(0).map((x, i) => i);
   }
 }
