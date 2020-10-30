9a83b0b2a39cfabbdadf3b7fac47b79652599f0d
Serge <sergiy.osadchyy@gmail.com>
2020-10-30 13:16:30 +0200

    refactor card

--- src/app/shared/controls/left-card/left-card.component.ts
+++ src/app/shared/controls/left-card/left-card.component.ts
@@ -10,6 +10,8 @@ export class LeftCardComponent {
   @Input() public description: string = 'UI\\UX Designer, Web Designer,\n' +
     'Mobile App Designer, UI\\UX Designer, Web Designer,Mobile App Designer';
   @Input() public city: string = 'Lviv, Ukraine';
+
+  @Input() public avatar: any = {src: 'assets/images/shared/controls/left-card/avatar.svg', alt: 'avatar'};
   @Input() public languages: Array<any> = [
     {src: 'assets/images/shared/controls/left-card/ukraine.svg', alt: 'ukrainian'},
     {src: 'assets/images/shared/controls/left-card/britain.svg', alt: 'english'},
@@ -21,6 +23,7 @@ export class LeftCardComponent {
     {src: 'assets/images/shared/controls/left-card/spain.svg', alt: 'spanish'},
     {src: 'assets/images/shared/controls/left-card/portugal.svg', alt: 'portuguese'}
   ];
+
 
   public stars: Array<number>;
 
