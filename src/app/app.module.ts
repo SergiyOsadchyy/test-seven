03caf394c51a10c9c1028c66aab1955f1428880d
Serge <sergiy.osadchyy@gmail.com>
2020-10-30 13:06:26 +0200

    add DemoComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LeftCardComponent } from './shared/controls/left-card/left-card.component';
+import { DemoComponent } from './pages/demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftCardComponent
+    LeftCardComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule
